fe70f96f78b21f7eac2a58fc961e549550a02eef
Septiana Yoga <septianayoga30@gmail.com>
2022-05-08 18:53:51 +0700

    adding initial navbar style

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Navbar from "../../components/navbar/Navbar";
+
+import "./home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 };
 
 export default Home;
