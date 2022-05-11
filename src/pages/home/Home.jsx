e9d1c7e6a526f9b7193e2a378975f1bfda766d3b
Septiana Yoga <septianayoga30@gmail.com>
2022-05-11 14:58:27 +0700

    initial code for featured items

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Featured from "../../components/featured/Featured";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
 
@@ -9,6 +10,10 @@ const Home = () => {
     <div>
       <Navbar />
       <Header />
+      <div className="homeContainer">
+        <Featured />
+        <h1 className="homeTitle"> Browse by property type</h1>
+      </div>
     </div>
   );
 };
